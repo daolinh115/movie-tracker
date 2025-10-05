f42fe50715911e4b7cc8eda2e522e5fc22f172d1
DaoLinh <daongoclinh1988@gmail.com>
2025-10-05 21:35:03 +0700

    after finishing tab

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const movieDetail = () => {
+  return (
+    <View>
+      <Text>Movie Detail</Text> 
+    </View>
+  )
+}
+
+export default movieDetail
